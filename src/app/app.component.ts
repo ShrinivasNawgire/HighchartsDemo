79c9bcaaef43719970ba950041faf9541ff3e320
Shrinivas Nawgire <shrinivasnawgire@gmail.com>
2021-11-23 19:06:52 +0530

    Added dataGrouping configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -111,6 +111,15 @@ export class AppComponent {
           series: {
             marker: {
               enabled: true
+            },
+            dataGrouping: {
+              enabled: true,
+              forced: true,
+              units: [[
+                'millisecond', // unit name
+                [1, 2, 5, 10, 20, 25, 50, 100, 200, 500] // allowed multiples
+              ]],
+              groupPixelWidth: 30
             }
           }
         },
@@ -129,7 +138,7 @@ export class AppComponent {
       },
       series: [...SERIES],
       boost: {
-        useGPUTranslations: true,
+        allowForce: true,
         seriesThreshold: 5
       },
     }
